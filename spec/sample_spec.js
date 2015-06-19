ec9e77e461de93083cd9fe494b96fd6a70a497c7
Matthias Benkort <matthias.benkort@gmail.com>
2015-06-19 17:41:53 +0200

    Add jasmine specs

--- spec/sample_spec.js
+++ spec/sample_spec.js
@@ -1,17 +1,61 @@
 /* Add Alloy in the global scope so that required module can have access */
 this.Alloy = require("alloy"); this._ = Alloy._; this.Backbone = Alloy.Backbone;
 
-describe("Sample test description", function () {
-    it("has been auto-generated", function () {
-        expect(true).toBe(true);
+var blurryView;
+
+describe("The might be initialized with options", function () {
+    beforeEach(function () {
+        blurryView = Alloy.createWidget("ts.blurryview");
+    });
+
+    afterEach(function () {
+        burryView = null;
+    });
+
+
+    it("Should raise an error when the background image is missing", function () {
+        var init = function () {
+            blurryView.init({
+                foregroundImage: "",
+                title: ""
+            });
+        };
+
+        expect(init).toThrow();
+    });
+
+    it("Should raise an error when the foreground image is missing", function () {
+        var init = function () {
+            blurryView.init({
+                backgroundImage: "",
+                title: ""
+            });
+        };
+
+        expect(init).toThrow();
+    });
+
+    it("Should raise an error when the title is missing", function () {
+        var init = function () {
+            blurryView.init({
+                backgroundImage: "",
+                foregroundImage: "",
+            });
+        };
+
+        expect(init).toThrow();
+    });
+
+    it("Should be initialized with some parameters", function () {
+        var init = function () {
+            blurryView.init({
+                title: "",
+                backgroundImage: "",
+                foregroundImage: "",
+                subtitle: ""
+            });
+        };
+
+        expect(init).not.toThrow();
     });
 });
-
-/* To launch,
- * - Start a TiShadow server : tishadow server
- * - Build the app as a tishadow bundle :  ti build -p xxx --appify
- * - Ask TiShadow : tishadow spec
- * 
- * or, simply launch 'gulp test:jasmine' which will do all tasks above
- * for you.
- */
